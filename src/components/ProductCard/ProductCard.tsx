a105cd58b97b71d863da2cb9ef608b205e88e751
velvet-noir <this.is.romniko@gmail.com>
2025-05-28 21:15:55 +0700

    добавил изображения на карточку

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,8 @@
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './ProductCard.module.css';
+import cardImage from '../../assets/no_image.png';
+
 
 interface ProductCardProps {
   id: string;
@@ -12,6 +14,7 @@ interface ProductCardProps {
 function ProductCard({ id, title, description, price }: ProductCardProps) {
   return (
     <Card className={styles.card}>
+      <Card.Img variant="top" src={cardImage} alt={title} />
       <Card.Body className={styles.cardBody}>
         <Card.Title className={styles.title}>{title}</Card.Title>
         <Card.Text className={styles.description}>{description}</Card.Text>

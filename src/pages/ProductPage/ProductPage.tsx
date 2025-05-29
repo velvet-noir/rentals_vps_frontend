f7f1a2ecba2a8c9c38066a8f212fd42550298c69
velvet-noir <this.is.romniko@gmail.com>
2025-05-29 20:46:12 +0700

    улучшил интерфейс страницы описания услуги

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
 import styles from './ProductPage.module.css';
@@ -7,27 +8,64 @@ function ProductPage() {
   const { id } = useParams<{ id: string }>();
 
   const product = {
-    id,
-    name: 'VPS Pro',
-    image: 'http://localhost:9000/r-vps-media/logo/vps.png',
-    mini_description: 'Улучшенный VPS-тариф для высоконагруженных проектов и стабильной работы.',
-    full_description: 'Этот VPS Pro сервер предоставляет расширенные возможности для хранения данных, высокой скорости доступа и надежности. Идеально подходит для средних и крупных проектов.',
-    price: '1199.00'
+    status: "success",
+    data: {
+      pk: id,
+      name: "VPS Pro",
+      image: "http://localhost:9000/r-vps-media/logo/vps.png",
+      mini_description: "Улучшенный VPS-тариф для высоконагруженных проектов и стабильной работы.",
+      price: "1199.00",
+      is_active: true,
+      specifications: [
+        {
+          id: 2,
+          description: "Тариф VPS Pro подойдёт для проектов, которым требуется высокая производительность, надёжность и гибкость. Идеален для интернет-магазинов, CRM-систем, медиасерверов и других ресурсоёмких приложений.",
+          processor: "2 vCPU (2.8 GHz)",
+          ram: "4 ГБ RAM",
+          disk: "100 ГБ SSD",
+          internet_speed: "до 250 Мбит/с",
+          service: 2
+        }
+      ]
+    }
   };
 
-  const imageUrl = product.image ? product.image : cardImage;
+  const imageUrl = product.data.image ? product.data.image : cardImage;
+  const spec = product.data.specifications[0];
+
+  const [showSpecs, setShowSpecs] = useState(false);
 
   return (
     <div className={styles.pageContainer}>
       <Card className={styles.productCard}>
         <Link to="/" className={styles.backButton}>← Назад</Link>
         <div className={styles.imageTitleWrapper}>
-          <Card.Img variant="top" src={imageUrl} alt={product.name} />
-          <Card.Title className={styles.title}>{product.name}</Card.Title>
+          <Card.Img variant="top" src={imageUrl} alt={product.data.name} />
+          <Card.Title className={styles.title}>{product.data.name}</Card.Title>
         </div>
         <Card.Body>
-          <Card.Text className={styles.description}>{product.full_description}</Card.Text>
-          <Card.Text className={styles.price}>{product.price} ₽ / месяц</Card.Text>
+          <Card.Text className={styles.description}>{spec.description}</Card.Text>
+          <Card.Text className={styles.price}>{product.data.price} ₽ / месяц</Card.Text>
+
+
+          <Button 
+            variant="outline-primary" 
+            onClick={() => setShowSpecs(!showSpecs)} 
+            className={styles.toggleButton}
+          >
+            {showSpecs ? 'Скрыть характеристики' : 'Показать характеристики'}
+          </Button>
+
+          {showSpecs && (
+            <div className={styles.specifications}>
+              <ul>
+                <li><strong>Процессор:</strong> {spec.processor}</li>
+                <li><strong>ОЗУ:</strong> {spec.ram}</li>
+                <li><strong>Диск:</strong> {spec.disk}</li>
+                <li><strong>Скорость интернета:</strong> {spec.internet_speed}</li>
+              </ul>
+            </div>
+          )}
         </Card.Body>
       </Card>
     </div>
